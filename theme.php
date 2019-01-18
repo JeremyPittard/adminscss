db0533ddb413d0ddbdf7647905dfb1abc03ba755
JeremyPittard <jeremy@jpittard.net>
2019-01-18 19:57:22 +0800

    added functionality to edit css of login page

--- theme.php
+++ theme.php
@@ -10,9 +10,14 @@ Author URI: http://example.com
 */
 
 function my_admin_theme_style() {
-    wp_enqueue_style('my-admin-theme', plugins_url('wp-admin.css', __FILE__));
+    wp_enqueue_style('my-admin-theme', plugins_url('admin.css', __FILE__));
 }
 add_action('admin_enqueue_scripts', 'my_admin_theme_style');
 add_action('login_enqueue_scripts', 'my_admin_theme_style');
 
-?>
+//allow stylesheet for login page
+function my_login_css() {
+  echo '<link rel="stylesheet" type="text/css" href="' .plugins_url('admin.css  ', __FILE__). '">';
+}
+
+add_action('login_head', 'my_login_css');
